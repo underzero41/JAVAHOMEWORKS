ede99c705fff7fa89b410733fb87110b72596f3b
1chello <underzero@bk.ru>
2023-04-13 14:31:55 +0300

    Add first project on java

---
+++ JavaEdu/File.java
@@ -0,0 +1,17 @@
+import java.io.FileWriter;
+import java.io.IOException;
+public class File {
+    public static void main(String[] args) {
+        try (FileWriter fw = new FileWriter("file.txt", false)){
+            fw.write("line 1");
+            fw.append('\n');
+            fw.append('2');
+            fw.append('\n');
+            fw.write("line 3");
+            fw.flush();
+        } catch (IOException ex){
+            System.out.println(ex.getMessage());
+        }
+       
+    }
+}
---
+++ JavaEdu/java_projects/Program.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+            //  var a = 123;
+            //  System.out.println(a);
+            //  var d = 123.24534;
+            //  System.out.println(d);
+            //  System.out.println(getType(a));
+            //  System.out.println(getType(d));
+            //  d =1022;
+            //  System.out.println(d);
+            // int i = 123_232_213;
+            // System.out.println(Byte.MIN_VALUE);   
+            
+            // byte b = Byte.parseByte("12");
+            // System.out.println(b);
+            // Scanner iScanner = new Scanner(System.in);
+            // System.out.println("int a: ");
+            // boolean flag =iScanner.hasNextInt();
+            // System.out.println(flag);
+            // int i = iScanner.nextInt();
+            // System.out.println(i);
+            // iScanner.close();
+            
+            // int a = 1, b = 2;
+            // int c = a + b;
+            // String res = a + "+" + b + "=" + c;
+            // System.out.println(res);
+
+            String s = "qwqe";
+            int a = 123;
+            String q = s + a;
+            System.out.println(q);
+        
+            //101
+            //010
+            //111
+           
+        }
+//   static String getType(Object o){
+//     return o.getClass().getSimpleName();
+//   }
+}
